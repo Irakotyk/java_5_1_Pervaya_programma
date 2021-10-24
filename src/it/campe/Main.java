366d7a1ab54cc18d9947b863064b34b358b756fb
Irakotyk <93030438+Irakotyk@users.noreply.github.com>
2021-10-24 10:30:09 +0200

    task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -2,7 +2,7 @@ package it.campe;
 
 public class Main {
 
-    public static void main(String[] args) {
-        //напишите тут ваш код
+    public static void main (String[] args) {
+        System.out.println("У меня все получитса");
     }
 }
